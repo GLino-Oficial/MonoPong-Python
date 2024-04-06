33bd5b7eae5e923f64c220a9e2ad53d04853442b
Lino <60303771+GLino-Oficial@users.noreply.github.com>
2024-04-06 19:35:10 +0000

    origin

--- main.py
+++ main.py
@@ -3,3 +3,21 @@
 #Py Pack - Visual Studio Code
 #Pygame -Via Terminal 
 #--------------------------------------#
+
+import pygame
+from pygame.locals import * 
+from sys import exit
+from random import randint
+
+
+#Opcional
+#pygame.mixer_music.set_volume(0.1)
+#musica_de_fundo = pygame.mixer.music('')
+#barulho_da_colisao = pygame.mixer.music('')
+#pygame.mixer.music.play(-1)
+#--------------------------------
+fonte = pygame.font.SysFont('arial',40,bold=True,italic=True)
+
+largura = 640
+altura = 480
+relogio = pygame.time.Clock()
